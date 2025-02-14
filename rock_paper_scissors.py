e0e58705ed66ef596dbf838556940b5c7cd51394
TomoCoder123 <thomasxdeng@gmail.com>
2025-02-14 05:02:49 -0800

    Computer Choice

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,8 +1,14 @@
+import random
 def main():
     print("Welcome to Rock Paper Scissors!")
     print("These are the Rules:Rock beats Scissors, Scissors beats Paper, Paper beats Rock")
+    choices = ["rock", "paper", "scissors"]
+
     player_choice = input("Enter rock, paper, or scissors: ").strip().lower()
     print(f"You chose: {player_choice}")
+    computer_choice = random.choice(choices)
+    print(f"The computer chose: {computer_choice}")
+
 
 if __name__ == "__main__":
     main()
