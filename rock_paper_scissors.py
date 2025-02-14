85ff3fb4a75ecd8d750f9f34486c50e3a99f914a
TomoCoder123 <thomasxdeng@gmail.com>
2025-02-14 05:01:02 -0800

    Added rules

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,5 +1,6 @@
 def main():
     print("Welcome to Rock Paper Scissors!")
+    print("These are the Rules:Rock beats Scissors, Scissors beats Paper, Paper beats Rock")
 
 if __name__ == "__main__":
     main()
