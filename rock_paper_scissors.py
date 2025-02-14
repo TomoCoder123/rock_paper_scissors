3983ef5429ffb4d519a22a2aec4690689db46308
TomoCoder123 <thomasxdeng@gmail.com>
2025-02-14 05:03:51 -0800

    implemented gameplay

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -9,6 +9,29 @@ def main():
     computer_choice = random.choice(choices)
     print(f"The computer chose: {computer_choice}")
 
-
+    if player_choice == computer_choice:
+        print("It's a tie!")
+    elif player_choice == "rock":
+        if computer_choice == "scissors":
+            print("You win!")
+        else:
+            print("You lose!")
+    elif player_choice == "scissors":
+        if computer_choice == "paper":
+            print("You win!")
+        else:
+            print("You lose!")
+    elif player_choice == "paper":
+        if computer_choice == "rock":
+            print("You win!")
+        else:
+            print("You lose!")
+    else:
+        print("Invalid input! You have not entered rock, paper or scissors, try again.")
+    play_again = input("Do you want to play again? yes/no: ").strip().lower()
+    if play_again == "yes":
+        main()
+    else:
+        print("Thanks for playing!")
 if __name__ == "__main__":
     main()
